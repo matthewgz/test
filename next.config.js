2f88532897287226039fdaa9de4dc57e4bab390a
Matthew Rosell Felix <matthewgz241@gmail.com>
2022-05-04 17:38:05 -0500

    qqeee

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  skipTypescriptChecking: true,
-  // or
-  typescript: {
-    transpileOnly: true, // same as ts-node --transpile-only
-  },
-  ignoreBuildErrors: true,
   swcMinify: true,
+  experimental: { esmExternals: true },
 };
 
 module.exports = nextConfig;
